0b06c925fa85100e9c9f4cf8cc2490d266d14233
kftse <kftse20031207@gmail.com>
2020-12-24 11:03:42 +0800

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # load your models here
+raise Exception("Model error")
 
 if os.getenv('CI') != '1':
     # init anything that requires a connection, these will not be checked
@@ -11,7 +12,7 @@ if os.getenv('CI') != '1':
 
 @app.route('/healthcheck')
 def healthcheck():
-    return "OK!1"
+    return "OK!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81)
+    app.run(host='0.0.0.0', port=80)
