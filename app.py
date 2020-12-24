4b3181d5e44876a1aef8b64692d8664e3c6a80a2
kftse <kftse20031207@gmail.com>
2020-12-24 11:14:24 +0800

    update

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 
+# Missing packages/import will fail healthcheck
 from flask import Flask
 app = Flask(__name__)
 
-# load your models here
+# TODO: load your models here
+# Exception will not pass healthcheck
+# raise Exception("Model error")
 
 if os.getenv('CI') != '1':
+    # TODO: init anything that requires a connection, these will not run in CI build
+    # This exception will pass healthcheck
     raise Exception("Network error")
-    # init anything that requires a connection, these will not be checked
     pass
 
 @app.route('/healthcheck')
@@ -15,4 +19,6 @@ def healthcheck():
     return "OK!"
 
 if __name__ == '__main__':
+    # Mounting incorrect port will not pass healthcheck
+    # app.run(host='0.0.0.0', port=81)
     app.run(host='0.0.0.0', port=80)
