e2c9212e825e7506f3f0305808bbc01390011224
kftse <kftse20031207@gmail.com>
2020-12-24 11:03:54 +0800

    update

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # load your models here
-raise Exception("Model error")
 
 if os.getenv('CI') != '1':
+    raise Exception("Network error")
     # init anything that requires a connection, these will not be checked
     pass
 
